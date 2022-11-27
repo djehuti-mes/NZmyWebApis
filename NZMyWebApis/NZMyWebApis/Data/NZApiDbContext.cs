1d201246945a3f1df7d8d13d7d2091b188fa989e
dalexis <dalexis@makayasistem.com>
2022-11-27 20:33:22 +0000

    End of section 2

---
+++ NZMyWebApis/NZMyWebApis/Data/NZApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZMyWebApis.Models.Domain;
+
+namespace NZMyWebApis.Data
+{
+    public class NZApiDbContext : DbContext
+    {
+        public NZApiDbContext(DbContextOptions<NZApiDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<Region>Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+    }
+}
---
+++ NZMyWebApis/NZMyWebApis/Models/Domain/Walk.cs
@@ -0,0 +1,14 @@
+﻿namespace NZMyWebApis.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+        //Navigation Property
+        public IEnumerable<Region> Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
